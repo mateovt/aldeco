a346bad1d7b0bfe4f376c9ec9af232ce216caaf2
Mateo Van Thienen <mateovanthienen@Mateos-MacBook-Pro.local>
2020-01-16 17:04:27 -0500

    changes

---
+++ main.js
@@ -0,0 +1,16 @@
+document.getElementById("redbutton").onclick = 
+function ScrollTo("redabogados") {
+  ScrollToResolver(document.getElementById("redabogados"));
+}
+
+function ScrollToResolver(elem) {
+  var jump = parseInt(elem.getBoundingClientRect().top * .2);
+  document.body.scrollTop += jump;
+  document.documentElement.scrollTop += jump;
+  if (!elem.lastjump || elem.lastjump > Math.abs(jump)) {
+    elem.lastjump = Math.abs(jump);
+    setTimeout(function() { ScrollToResolver(elem);}, "100");
+  } else {
+    elem.lastjump = null;
+  }
+}
